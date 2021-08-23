6ffb92df4a65290e1371a4f4a694110b99ae2e2d
anjalibabar01 <anjalibabar9192@gmail.com>
2021-08-23 17:44:04 +0530

    java

---
+++ java2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java;
+
+/**
+ * @author Anjali babar
+ *
+ */
+public class java2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
