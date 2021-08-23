543c8ce2dc6599a452efa138c0f84212d453151b
anjalibabar01 <89136480+anjalibabar01@users.noreply.github.com>
2021-08-23 18:05:56 +0530

    java

--- java2.java
+++ java2.java
@@ -14,7 +14,7 @@ public class java2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello java");
+		System.out.println(" \"can we print '\\' with system.out.println() statement? ");
 		
 
 	}
