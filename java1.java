7df076f21209c5c84b07e02deebbf5c95cb79129
anjalibabar01 <anjalibabar9192@gmail.com>
2021-08-24 18:18:01 +0530

    java

---
+++ java1.java
@@ -0,0 +1,31 @@
+/**
+ * 
+
+ */
+package com.java;
+
+/**
+ * @author Anjali babar
+ *
+ */
+public class java1 {
+
+	/**
+		 * @author Anjali babar
+		 *
+		 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("anuj "+"anuj@gmail.com "+"101 "+"9224466227");
+		System.out.println("ankit "+"ankit@mail.com "+"102 "+"9224466222");
+		System.out.println("sohil "+"sohil@gmail.com "+"103 "+"9224466223");
+		System.out.println("gunjan "+"gunjan@gmail.com "+"104 "+"9224466224");
+		System.out.println("aman "+"aman@gmail.com "+"105 "+"9224466225");
+	}
+}
+		
+		
+		
+		
+
